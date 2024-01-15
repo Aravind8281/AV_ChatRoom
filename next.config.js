ca4e0aff4905b3f3b8e60e9434ef1c67da3be117
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-15 12:11:33 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+          "uploadthing.com",
+          "utfs.io"
+        ]
+      }
+}
+
+module.exports = nextConfig
